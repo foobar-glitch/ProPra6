c0d2dff77bdd823a8aa399fc1280e9d57600c161
mar-abc123 <mygarbagemailbox1@gmail.com>
2024-12-03 19:52:42 +0100

    sketched out OfficeGen

--- src/OfficeGen.java
+++ src/OfficeGen.java
@@ -1,2 +1,37 @@
 public class OfficeGen {
+    int id;
+    double areaOfAdjacentRooms;
+    // usableRooms >= 1
+    // unusableRooms
+
+    public OfficeGen(int id, double areaOfAdjacentRooms) {
+        this.id = id;
+        this.areaOfAdjacentRooms = areaOfAdjacentRooms;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getAreaOfAdjacentRooms() {
+        return areaOfAdjacentRooms;
+    }
+
+    public double totalArea() {
+        // add area of usable rooms to this
+        return areaOfAdjacentRooms;
+    }
+
+    public void addRoom() {
+        // how to add usable/unusable room? how to differenciate??
+    }
+
+    public void removeRoom() {
+        // how to remove usable/unusable room? how to differenciate??
+    }
+
+    // Ändern der Informationen von Räumen wie oben beschrieben
+
+    // Methoden zum Berechnen folgender (statistischer) Werte:
+    // ...
 }
