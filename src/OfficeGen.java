ecc0623389e13e5449aec89434e295c2d71e6468
hamed <asamihamed@gmail.com>
2024-12-07 11:06:06 +0100

    Is that the gen they want?

--- src/OfficeGen.java
+++ src/OfficeGen.java
@@ -1,4 +1,26 @@
 public class OfficeGen<K, V extends Space> implements ContainerInterface<Integer> {
+    private final int id;
+    private final LinkedMapGen<K, V> rooms;
+
+    public OfficeGen(int id, LinkedMapGen<K, V> rooms) {
+        this.id = id;
+        this.rooms = rooms;
+    }
+
+    public int getId() {return id;}
+
+    public double getAreaOfAdjacentRooms() {
+        double areaSideRooms = 0.0;
+        V space;
+        for (LinkedMapGen<K, V> currentNode = rooms; currentNode != null; currentNode = currentNode.getNext()) {
+            space = currentNode.getValue();
+            Room room = space.room();
+            if (!(space instanceof UsableSpace)) {
+                areaSideRooms += room.getLength() * room.getWidth();
+            }
+        }
+        return areaSideRooms;
+    }
 
 
     @Override
