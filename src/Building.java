99bb0335fde0a58d313fb3be32c23dc7be0e465a
julianschloer <e12322028@student.tuwien.ac.at>
2024-12-08 21:11:06 +0100

    Added and changed Comments

--- src/Building.java
+++ src/Building.java
@@ -3,20 +3,39 @@ public class Building implements StringContainerInterface{
     private final String name;
     private final OfficeLinkedMap map = new OfficeLinkedMap();
 
+    /**
+     * Creates new Building
+     * @param name name!=null, Unique Name of (this) Building
+     */
     public Building(String name) {
         this.name = name;
     }
 
-    public void add(Office office){
+    /**
+     * Adds office to this Building if no office with the same unique id is in this
+     * @param office office!=null, office to be added to this
+     * @return  TRUE if office is added to this, FALSE if office was not added to this
+     *          (because this already contained an office with same id)
+     */
+    public boolean add(Office office){
         int id = office.id();
-        if(map.get(id) != null) return;
+        if(map.get(id) != null) return false;
         map.put(office.id(), office);
+        return true;
     }
 
+    /**
+     * Removes Office from this
+     * @param id unique id of Office which should be removed from this
+     * @return Office which was removed or Null if no such Office was in this
+     */
     public Office remove(int id){
         return map.remove(id);
     }
 
+    /**
+     * @return Unique Name of this
+     */
     public String getName(){return this.name;}
 
     @Override
--- src/Company.java
+++ src/Company.java
@@ -3,18 +3,37 @@ public class Company implements StringContainerInterface{
     private final String name;
     private final BuildingLinkedMap map = new BuildingLinkedMap();
 
+
+    /**
+     * Creates new Company
+     * @param name name!=null, Unique Name of (this) Company
+     */
     public Company(String name) {
         this.name = name;
     }
 
+    /**
+     * Adds building to this Building if no office with the same unique name is in this
+     * @param building building!=null, building to be added to this
+     * @return  TRUE if building is added to this, FALSE if building was not added to this
+     *          (because this already contained an building with same name)
+     */
     public void add(Building building){
         String key = building.id();
         if(map.get(key) != null) return;
         map.put(key, building);
     }
 
+    /**
+     * Removes Building from this
+     * @param name unique name of Building which should be removed from this
+     * @return Building which was removed or Null if no such Building was in this
+     */
     public Building remove(String name){return map.remove(name);}
 
+    /**
+     * @return Unique Name of this
+     */
     public String getName(){return this.name;}
 
     @Override
