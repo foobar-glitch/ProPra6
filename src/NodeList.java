7cc1b86d12a5112dccc163f659aae32cd19660bb
julianschloer <e12322028@student.tuwien.ac.at>
2024-12-08 17:51:57 +0100

    Refractored Code

--- src/NodeList.java
+++ src/NodeList.java
@@ -1,89 +1,172 @@
+// TODO ist das hier eine geschachtelte Klasse?
+//  "Schreiben Sie (abgesehen von geschachtelten Klassen) nicht mehr als eine
+//  Klasse in jede Datei. Verwenden Sie keine Umlaute in Dateinamen.
+//  Achten Sie darauf, dass Sie keine Java-Dateien abgeben, die nicht zu Ihrer
+//  Lösung gehören (alte Versionen, Reste aus früheren Versuchen, etc.)."
+
+import java.util.NoSuchElementException;
+
+/**
+ * Implementation of a simple LinkedList
+ */
 public class NodeList {
-    // Node class representing an element of the LinkedList
-    public static class Node {
-        String data;
-        Node next;
 
-        Node(String data) {
-            this.data = data;
-            this.next = null;
-        }
+    RoomNode start = null;
+
+    /**
+     * Adds elem to NodeList
+     * @param elem (elem!=null)
+     */
+    public void add(Room elem){
+        if(start == null){start = new RoomNode(elem); return;}
+        RoomNode tmp = start;
+        while(tmp.next != null){tmp = tmp.next;}
+        tmp.next = new RoomNode(elem);
     }
 
-    // Head of the linked list
-    private Node head;
-
-    // Constructor
-    public NodeList() {
-        this.head = null;
-    }
-
-
-    // Method to add a string to the end of the list
-    public void add(String data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode; // If list is empty, make the new node the head
-            return;
-        }
-        Node current = head;
-        while (current.next != null) { // Traverse to the last node
-            current = current.next;
-        }
-        current.next = newNode; // Add the new node at the end
-    }
-
-
-    // Method to remove a string (first occurrence) from the list
-    public boolean remove(String data) {
-        if (head == null) return false; // List is empty
-
-        // If the head is the node to remove
-        if (head.data.equals(data)) {
-            head = head.next;
-            return true;
-        }
-
-        // Search for the node to remove
-        Node current = head;
-        while (current.next != null && !current.next.data.equals(data)) {
-            current = current.next;
-        }
-
-        // If the node was found, remove it
-        if (current.next != null) {
-            current.next = current.next.next;
-            return true;
-        }
-        return false; // Node not found
-    }
-
-    // Method to check if the list contains a string
-    public boolean contains(String data) {
-        Node current = head;
-        while (current != null) {
-            if (current.data.equals(data)) return true;
-            current = current.next;
+    /**
+     * Removes first Element equal to this from this NodeList
+     * @param elem (elem!=null) elem to remove
+     * @return true => NodeList contained elem || false => NodeList didn't contain elem
+     */
+    public boolean remove(Room elem){
+        if(start == null) return false;
+        RoomNode tmp = start;
+        // Special case if start is removed
+        if(start.value.equals(elem)){start = start.next; return true;}
+        // Else
+        while(tmp.next != null){
+            // If tmp.next equals elem, remove it and fix list
+            if(tmp.next.value.equals(elem)){
+                tmp.next = tmp.next.next;
+                return true;
+            }
+            // Else go to next Node
+            tmp = tmp.next;
         }
         return false;
     }
 
-    // Method to get the size of the linked list
-    public int size() {
-        int count = 0;
-        Node current = head;
-        while (current != null) {
-            count++;
-            current = current.next;
+    /**
+     * Removes all occurrences of elem in NodeList
+     * @param elem (!= null)
+     * @return true (elem existed in NodeList) || false (elem didn't exist in NodeList)
+     */
+    public boolean removeAll(Room elem){
+        if(!contains(elem)) return false;
+
+        boolean state = true;
+        while (state){
+            state = remove(elem);
         }
-        return count;
+
+        return true;
     }
 
-    /*
-    public void iterateWithFor() {
-        for (Node current = head; current != null; current = current.next) {
-            System.out.println(current.data);
-        }
-    }
+    /**
+     * Does NodeList contain this Object?
+     * @param elem (elem!= null)
+     * @return True (NodeList contains elem); False (NodeList doesn't contain elem)
      */
+    public boolean contains(Room elem){
+        if(start== null) return false;
+        RoomNode tmp = start;
+        while (tmp != null){
+            if(tmp.value.equals(elem)) return true;
+            tmp = tmp.next;
+        }
+        return false;
+    }
+
+    /**
+     * Retrieves Element at Index index, if there is no elem at index return null
+     * @param index Index of element to be retrieved
+     * @return Elem or Null if no element exists at this index
+     */
+    public Room get(int index){
+        if(start == null) return null;
+
+        int i = 0;
+        RoomNode tmp = start;
+        while (tmp != null){
+            if(i++ == index) return tmp.value;
+            tmp = tmp.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * @return Number of Elements in NodeList
+     */
+    public int length(){
+
+        int len = 0;
+        if(start == null) return len;
+
+        RoomNode tmp = start;
+        while(tmp != null){len++; tmp=tmp.next;}
+
+        return len;
+    }
+
+    /**
+     * Live-Iterator over all elements in NodeList
+     * @return Iterator
+     */
+    public Iterator iterator(){
+        return new RoomNodeListIterator(start);
+    }
+
+    /**
+     * @return String in Array Style, containing all elements in NodeList [x0, x1, x2, ...]
+     */
+    @Override
+    public String toString(){
+        if(start == null) return "[]";
+        StringBuilder stringBuilder = new StringBuilder();
+        RoomNode tmp = start;
+        while(tmp != null){
+            stringBuilder.append(tmp.value).append(", ");
+            tmp = tmp.next;
+        }
+        return "[" + stringBuilder.substring(0, stringBuilder.length()-2) + "]";
+    }
+
+}
+
+/**
+ * Simple Node Implementation
+ * @param <X> Type of Object to be value of Node
+ */
+class RoomNode{
+
+    RoomNode next = null;
+    Room value = null;
+
+    public RoomNode(Room value){this.value = value;}
+
+}
+
+/**
+ * Simple Iterator iver Room objects implementation
+ */
+class RoomNodeListIterator implements Iterator {
+
+    RoomNode current;
+
+    public RoomNodeListIterator(RoomNode start){this.current = start;}
+
+    @Override
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    @Override
+    public Room next() {
+        if(!hasNext()) throw new NoSuchElementException();
+        Room value = current.value;
+        current = current.next;
+        return value;
+    }
 }
