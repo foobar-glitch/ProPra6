dbda2dc5e2b1cdd233e79258e8418903903beb2f
julianschloer <e12322028@student.tuwien.ac.at>
2024-12-08 21:36:40 +0100

    Added and changed Comments

--- src/IteratorGen.java
+++ src/IteratorGen.java
@@ -1,7 +1,16 @@
+import java.util.NoSuchElementException;
+
 public interface IteratorGen<X> {
 
+    /**
+     * @return True if this.next() returns an Element
+     */
     boolean hasNext();
 
+    /**
+     * @return Next Element from Iterator
+     * @throws NoSuchElementException if no next element exists
+     */
     X next();
 
 }
--- src/WorkSpace.java
+++ src/WorkSpace.java
@@ -1,8 +1,16 @@
 public class WorkSpace implements Space, UsableSpace {
-    int workStations;
+    private final int workStations;
+
+    /**
+     * Creates new object of this
+     * @param workStations workStations >= 0, number of workStations in Room
+     */
     public WorkSpace(int workStations){
         this.workStations = workStations;
     }
 
+    /**
+     * @return number of workStations in Room
+     */
     public int workStations(){return this.workStations;}
 }
