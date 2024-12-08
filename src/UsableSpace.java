1a0621391e285443253a3676fd227349246ad406
julianschloer <e12322028@student.tuwien.ac.at>
2024-12-08 21:41:16 +0100

    Added and changed Comments

--- src/UsableSpace.java
+++ src/UsableSpace.java
@@ -1,1 +1,5 @@
+/**
+ * Interface which needs to be implemented by all Room Usage classes which have "special" functions,
+ * meaning they are not any kind of adjacent rooms
+ */
 public interface UsableSpace {}
